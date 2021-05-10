8099cf56484205708263cd5c3f345abeba325435
DennyNice <dennynice@icloud.com>
2021-05-10 21:57:07 +0200

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title-block') Page about us @endsection
+
+@section('content')
+    <h1>Page about us</h1>
+@endsection
+
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title-block') Contact Page @endsection
+
+@section('content')
+    <h1>Contact Page</h1>
+@endsection
